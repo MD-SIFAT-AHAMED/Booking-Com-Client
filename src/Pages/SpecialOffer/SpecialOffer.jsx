0fe252e1828c740ebe164430216071e179335b59
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-08-08 23:52:51 +0600

    opper section some change

--- src/Pages/SpecialOffer/SpecialOffer.jsx
+++ src/Pages/SpecialOffer/SpecialOffer.jsx
@@ -18,7 +18,7 @@ const SpecialOffer = ({ onClose }) => {
           className="w-full h-64 rounded-md mb-4"
         />
         <h2 className="text-2xl font-bold mb-2 text-center text-red-600">
-          🎉 Special Offer!
+          🎉🎉 Special Offer!
         </h2>
         <p className="text-gray-700 text-center">
           Get 25% OFF your first room booking. Today only!
