3b215a092ae4e3d78f26356f301599e6545ac79d
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-08-10 11:24:54 +0600

    Review section and hotel location design update

--- src/Pages/Home/HotelLocation.jsx
+++ src/Pages/Home/HotelLocation.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import HeaderSection from "../../Components/HeaderSection/HeaderSection";
 
 const HotelLocation = () => {
   const position = [23.7906485, 90.412521];
 
   return (
     <div className="max-w-screen-2xl w-11/12 mx-auto my-10 z-0">
-      <h2 className="text-2xl text-center my-4 md:text-4xl font-bold">
-        Hotel Location
-      </h2>
+      <HeaderSection
+        title="Hotel Location"
+        subtitle="Find the exact spot of your stay with our interactive map"
+      />
       <MapContainer
         center={position}
         zoom={13}
         scrollWheelZoom={false}
-        style={{ height: "450px", width: "100%" , zIndex: 0}}
+        style={{ height: "450px", width: "100%", zIndex: 0 }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
